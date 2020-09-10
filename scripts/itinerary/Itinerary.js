35448cea50240b25bbed92ef642f742752f1cf3a
Ember Parr <ember-devops@gmail.com>
2020-09-10 15:22:59 -0500

    sidebar functionality works with default inputs

--- scripts/itinerary/Itinerary.js
+++ scripts/itinerary/Itinerary.js
@@ -1,1 +1,42 @@
-//creates HTML for aside
+/* 
+    creates HTML for aside containing cards for each saved itinerary in LOCAL database
+*/
+
+export const ItineraryHTML = (anObject) => {
+  return `
+        <section class="card-itinerary">
+            <p class="card-title">Itinerary For: ${anObject.park}</p>
+            <p>• Place To Eat: ${anObject.eatery}<br>
+            • Bizzare Attraction to Visit: ${anObject.attraction}</p>
+            <p class="caption">Date Saved: ${new Date(
+              anObject.date
+            ).toLocaleDateString("en-US")}</p>
+            
+        </section>
+        `;
+};
+
+/* all code below will REPLACE code above & is to be used ONLY if we can capture data on:
+    1. has cell service y/n 
+    2. caution alerts 
+    3. webcams
+*/
+
+/*
+export const ItineraryHTML = (anObject) => {
+  return `
+        <section class="card-itinerary">
+            <p class="card-title">Itinerary For: ${anObject.park}</p>
+            <p>• Place To Eat: ${anObject.eatery}<br>
+            • Bizzare Attraction to Visit: ${anObject.attraction}<br>
+            • Cell Service: ${anObject.cellService} <br>
+            • Active Caution Alerts: ${anObject.cautionAlert}<br>
+            • Webcam Info: ${anObject.webcam}</p>
+            <p class="caption">Date Saved: ${new Date(
+              anObject.date
+            ).toLocaleDateString("en-US")}</p>
+            
+        </section>
+        `;
+};
+*/
--- scripts/itinerary/ItinerarySave.js
+++ scripts/itinerary/ItinerarySave.js
@@ -1,3 +1,46 @@
-// event to save itinerary when button pressed
-// creates itinerary object & sends to json database
-// dispatches/invokes customEvent
+/*
+event to save itinerary when button pressed
+creates itinerary object & sends to json database
+dispatches/invokes customEvent
+*/
+
+import {
+  getItinerary,
+  useItinerary,
+  saveItinerary,
+} from "./ItineraryProvider.js";
+import { ItineraryList } from "./ItineraryList.js";
+import { ItineraryHTML } from "./Itinerary.js";
+
+const elementTarget = document.querySelector(".savedItinerary");
+const eventHub = document.querySelector(".container");
+
+eventHub.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.classList.contains("itinerarySaveBtn")) {
+    console.log("button clicked");
+    const parkChosen = "PULL FROM PARK DROPDOWN MENU";
+    const eateryChosen = "PULL FROM EATERY DROPDOWN MENU";
+    const attractionChosen = "PULL FROM ATTRACTION DROPDOWN MENU";
+
+    if (attractionChosen.value === "") {
+      window.alert("please select an attraction");
+    } else if (eateryChosen.value === "") {
+      window.alert("please select an eatery");
+    } else {
+      const newItinerary = {
+        park: parkChosen,
+        eatery: eateryChosen,
+        attraction: attractionChosen,
+        date: Date.now(),
+      };
+      saveItinerary(newItinerary);
+      elementTarget.innerHTML += ItineraryHTML(newItinerary);
+    }
+  }
+});
+
+export const savedItinerary = () => {
+  getItinerary().then(() => {
+    ItineraryList(useItinerary());
+  });
+};
