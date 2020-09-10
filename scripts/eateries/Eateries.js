58540ac4b7f466466fd2999defccdc87d6b51c36
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-10 09:19:47 -0500

    updated Eateries and EaterySelect

--- scripts/eateries/Eateries.js
+++ scripts/eateries/Eateries.js
@@ -1,1 +1,10 @@
 // creates HTML to go on dom with the selected eatery (selected in dropdown from EaterySelect.js)
+
+export const EateriesHTML = (eateryObj) => {
+    return `
+        <div id="eatery--${eateryObj.id}">
+            <h2>${eateryObj.businessName}</h2>
+            <button class="eatery--button" id="btn--${eateryObj.id}">Details</button>
+        </div>
+    `
+}
--- scripts/eateries/EaterySelect.js
+++ scripts/eateries/EaterySelect.js
@@ -2,3 +2,42 @@
 // once an eatery is selected, should display on preview with option to see details
 // when 'save' button clicked on DOM, eatery saves with itinerary in DataBase
 // will dispatch an event.... hopefully.
+
+import { useEateries, getEateries } from './EateryProvider.js'
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".eateriesDropdown")
+const buttonTarget = document.querySelector(".eatery--button")
+
+eventHub.addEventListener("change", event => {
+    if (event.target.id === "eaterySelect") {
+        const customEvent = new CustomEvent("eateryChosen", {
+            detail: {
+                eateryId: event.target.value
+            }
+        })
+
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+const render = eateriesCollection => {
+    contentTarget.innerHTML = `
+        <select class="dropdown" id="eaterySelect">
+            <option value="0">Choose an Eatery</option>
+            ${
+        eateriesCollection.map(eateryObj => {
+            return `<option value="${eateryObj.name}">${eateryObj.name}</option>`
+        }).join("")
+        }
+        </select>
+    `
+}
+
+export const AttractionSelect = () => {
+    getEateries()
+        .then(() => {
+            const eateries = useEateries()
+            render(eateries)
+        })
+}
