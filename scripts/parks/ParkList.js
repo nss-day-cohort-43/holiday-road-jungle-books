c93da2c7545da3d18d48ab5083cee5415b032a68
Brady Logan <brady.drew.logan@gmail.com>
2020-09-10 08:58:49 -0500

    display park name when selected

--- scripts/parks/ParkList.js
+++ scripts/parks/ParkList.js
@@ -1,8 +1,13 @@
+import { Park } from './Parks.js'
+
 // render the HTML onto the DOM
 // import Parks.js
 // listen for customEvent from ParkSelect
 const eventHub = document.querySelector('.container')
 
 eventHub.addEventListener('parkSelected', event => {
-    debugger;
+    var contentTarget = document.querySelector('.itineraryTitle')
+    if(event.detail.hasOwnProperty('name')){
+        contentTarget.innerHTML = Park(event.detail.name)
+    }
 })
--- scripts/parks/Parks.js
+++ scripts/parks/Parks.js
@@ -1,1 +1,4 @@
 // creates HTML to go on dom with the selected park (selected in dropdown from ParkSelect.js)
+export const Park = (park) => {
+    return `<h2>${park}</h2>`
+}
