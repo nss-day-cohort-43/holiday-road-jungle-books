3e4d823fa4d7b2449be15855b5e0f648ee8b2647
Brady Logan <brady.drew.logan@gmail.com>
2020-09-11 10:02:21 -0500

    updated main.js and main.css for updated master

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,13 @@ import './parks/ParkSelect.js'
 import './parks/ParkList.js'
 import { WeatherList } from "./weather/WeatherList.js"
 import { savedItinerary } from './itinerary/ItinerarySave.js'
+import { EaterySelect } from './eateries/EaterySelect.js'
+import { AttractionSelect } from './attractions/AttractionSelect.js'
+import './eateries/EateryList.js'
+import './attractions/AttractionList.js'
 
 WeatherList();
 savedItinerary();
 StateSelect();
+AttractionSelect();
+EaterySelect();
