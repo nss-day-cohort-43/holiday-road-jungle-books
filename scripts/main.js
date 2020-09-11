433e9054a611b2f4705d4f5f97bf99d82769a98c
Brady Logan <brady.drew.logan@gmail.com>
2020-09-11 09:35:06 -0500

    upated from merged master

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,9 @@ import { StateSelect } from './parks/states/StateSelect.js';
 import './parks/activites/ActivitySelect.js'
 import './parks/ParkSelect.js'
 import './parks/ParkList.js'
+import { WeatherList } from "./weather/WeatherList.js"
+import { savedItinerary } from './itinerary/ItinerarySave.js'
 
-
+WeatherList();
+savedItinerary();
 StateSelect();
