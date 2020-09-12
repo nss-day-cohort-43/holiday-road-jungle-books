dbc9a427c4626bc88343ed0259a7a2c6b1164fe2
Cornelia Johnson <corneliaj91@gmail.com>
2020-09-11 21:53:00 -0400

    added a default background image

--- scripts/parks/image/ParkImageComponent.js
+++ scripts/parks/image/ParkImageComponent.js
@@ -1,15 +1,9 @@
+// render the HTML onto the DOM
+// listen for customEvent from ParkSelect
 import "../ParkList.js";
 import { usePark } from "../ParkProvider.js";
-
-// render the HTML onto the DOM
-// import Parks.js
-// listen for customEvent from ParkSelect
 const eventHub = document.querySelector(".container");
-//reders to page on load
 const contentTarget = document.querySelector(".heroSection");
-contentTarget.innerHTML = ` <div class="heroImageFull">
-<img class="heroImage" src="../../imgs/defaultPark.jpg"  alt="nationalpark" >
-</div>`;
 
 eventHub.addEventListener("parkSelected", (event) => {
   render();
@@ -17,17 +11,18 @@ eventHub.addEventListener("parkSelected", (event) => {
 
 const render = () => {
   if (usePark().images.length !== 0) {
-    contentTarget.innerHTML = ` <div class="externalImage">
+    contentTarget.innerHTML = ` <div class="heroImageFull">
     <img class="heroImage" src="${usePark().images[0].url}"  alt="${
-      usePark().images[0].title
-    }" >
-      <div class="bottom-left"><p>${usePark().description}</p></div>
+      usePark().images[0].altText
+    }">
+      <div class="bottom-left"><p>${usePark().images[0].caption}</p></div>
     </div>`;
   } else {
-    //use default image
-    document.querySelector(".heroImageFull").innerHTML += `
-    <div class="centered"><h3>No Image of ${usePark().fullName}</h3></div>
-    <div class="bottom-left"><p>${usePark().description}</p></div>
-  `;
+    //use default background-image
+    contentTarget.innerHTML += `<div class="centered"><h2>No Image of ${
+      usePark().fullName
+    }</h2></div>`;
   }
 };
+
+//GOAL: Add loader until image is complete
