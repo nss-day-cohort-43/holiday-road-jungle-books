8427d7ef28398dcfd33d791ffa53bd14395e6ab7
Cornelia Johnson <corneliaj91@gmail.com>
2020-09-14 12:02:44 -0400

    changed alt text position

--- scripts/parks/image/ParkImageComponent.js
+++ scripts/parks/image/ParkImageComponent.js
@@ -1,13 +1,12 @@
 // render the HTML/Image onto the DOM
 // listen for customEvent from ParkSelect
-import "../ParkList.js";
 import { usePark } from "../ParkProvider.js";
 const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".heroSection");
 
 //renders when page loads
 contentTarget.innerHTML = ` <div class="heroImageFull">
-<img class="heroImage" src="../../imgs/defaultPark.jpg"> alt="default image"`;
+<img class="heroImage hide-text" alt="default image" src="../../imgs/defaultPark.jpg">`;
 
 eventHub.addEventListener("parkSelected", (event) => {
   render();
@@ -16,15 +15,15 @@ eventHub.addEventListener("parkSelected", (event) => {
 const render = () => {
   if (usePark().images.length !== 0) {
     contentTarget.innerHTML = ` <div class="heroImageFull">
-    <img class="heroImage" src="${usePark().images[0].url}"  alt="${
+    <img class="heroImage hide-text" alt="${
       usePark().images[0].altText
-    }">
+    }" src="${usePark().images[0].url}">
       <div class="bottom-left"><p>${usePark().images[0].caption}</p></div>
     </div>`;
   } else {
     //use default background-image and add text
     contentTarget.innerHTML = ` <div class="heroImageFull">
-    <img class="heroImage" src="../../imgs/defaultPark.jpg" alt="default image">
+    <img class="heroImage hide-text" alt="default image" src="../../imgs/defaultPark.jpg">
     <div class="centered"><h2>No Image of ${usePark().fullName}</h2></div>
     </div>`;
   }
