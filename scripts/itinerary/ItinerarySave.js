cbebb93d54f440104927db82f6cc7a772fc05778
Ember Parr <ember-devops@gmail.com>
2020-09-14 13:22:54 -0500

    fixed issue with save button

--- scripts/itinerary/ItinerarySave.js
+++ scripts/itinerary/ItinerarySave.js
@@ -15,27 +15,34 @@ import { usePark } from "../parks/ParkProvider.js";
 import { useMatchingEatery } from "../eateries/EateryList.js";
 import { useMatchingAttraction } from "../attractions/AttractionList.js";
 
+let chosenPark = {};
+let eateryChosen = {};
+let attractionChosen = {};
+
+export const saveButtonEnable = () => {
+  const saveBtn = document.querySelector(".itinerarySaveBtn")
+  if (chosenPark === {} || eateryChosen === {} || attractionChosen === {} ) {
+      saveBtn.disabled = true
+  } else {
+    saveBtn.disabled = false
+  }
+}
+
+
 const elementTarget = document.querySelector(".savedItinerary");
 const eventHub = document.querySelector(".container");
 //event creates a new saved itinerary in local database
 eventHub.addEventListener("click", (clickEvent) => {
   if (clickEvent.target.classList.contains("itinerarySaveBtn")) {
-    let chosenPark = usePark();
-    
+    chosenPark = usePark();
     let chosenEatery = useMatchingEatery();
     let eateryObject = chosenEatery[0];
-    const eateryChosen = eateryObject;
-
-    
+    eateryChosen = eateryObject;
     let chosenAttraction = useMatchingAttraction();
     let attractionObject = chosenAttraction[0];
-    const attractionChosen = attractionObject;
+    attractionChosen = attractionObject;    
 
-    if (attractionChosen === "") {
-      window.alert("please select an attraction");
-    } else if (eateryChosen === "") {
-      window.alert("please select an eatery");
-    } else {
+    if (chosenPark != 0 && chosenAttraction != 0 && chosenEatery != 0) {
       const newItinerary = {
         park: chosenPark,
         eatery: eateryChosen,
@@ -44,6 +51,8 @@ eventHub.addEventListener("click", (clickEvent) => {
       };
       saveItinerary(newItinerary);
       elementTarget.innerHTML += ItineraryHTML(newItinerary);
+    } else {
+      window.alert("please select park, eatery & attraction")
     }
   }
 });
@@ -52,3 +61,5 @@ export const savedItinerary = () => {
     ItineraryList(useItinerary());
   });
 };
+
+
