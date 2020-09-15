09e6203bc9da6911f408a7d6fad40d108dda4c33
Brady Logan <brady.drew.logan@gmail.com>
2020-09-15 11:10:38 -0500

    can now add an attraction from the search results

--- scripts/search/SearchResults.js
+++ scripts/search/SearchResults.js
@@ -1,4 +1,7 @@
 import { dispatchParkEvent, getPark, usePark } from '../parks/ParkProvider.js'
+import { useEateries, getEateries, dispatchEateryEvent } from '../eateries/EateryProvider.js'
+import { useAttractions, getAttractions, dispatchAttractionEvent } from '../attractions/AttractionProvider.js'
+
 
 const eventHub = document.querySelector('.container')
 
@@ -14,7 +17,7 @@ eventHub.addEventListener('customSearch', event =>{
 
             ${event.detail.attractions.map(attraction => {
                 return `
-                    <li>${attraction.name}</li>
+                    <li>${attraction.name}<button id="${attraction.name}" class="addAttractionBtn">Add</button></li>
                 `
             }).join("")}
 
@@ -30,12 +33,16 @@ eventHub.addEventListener('customSearch', event =>{
 eventHub.addEventListener("click", event => {
     if(event.target.classList.contains('selectParkBtn')){
         const parkCode = event.target.id
-        debugger;
         getPark(parkCode)
         .then(_ => {
             const park = usePark()
-            debugger;
             dispatchParkEvent(park)
         })
     }
+
+    if(event.target.classList.contains('addAttractionBtn')){
+        const attractionID = event.target.id
+        dispatchAttractionEvent(attractionID)
+    }
+
 })
