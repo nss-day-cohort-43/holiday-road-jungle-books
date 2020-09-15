63e17754477c43b5b34ea3f6c405e2ce9bd51776
Brady Logan <brady.drew.logan@gmail.com>
2020-09-15 10:31:43 -0500

    can now select a park from the search results

--- scripts/search/SearchResults.js
+++ scripts/search/SearchResults.js
@@ -1,12 +1,14 @@
+import { dispatchParkEvent, getPark, usePark } from '../parks/ParkProvider.js'
+
 const eventHub = document.querySelector('.container')
 
 eventHub.addEventListener('customSearch', event =>{
     if("parks" in event.detail && "eateries" in event.detail && "attractions" in event.detail){
         const contentTarget = document.querySelector('.searchResults')
-        contentTarget.innerHTML += `
+        contentTarget.innerHTML = `
             ${event.detail.parks.map(park => {
                 return `
-                    <li>${park.fullName}<button class="selectParkBtn">Add</button></li>
+                    <li>${park.fullName}<button id="${park.parkCode}" class="selectParkBtn">Add</button></li>
                 `
             }).join("")}
 
@@ -22,5 +24,18 @@ eventHub.addEventListener('customSearch', event =>{
                 `
             }).join("")}
         `
+    }
+})
+
+eventHub.addEventListener("click", event => {
+    if(event.target.classList.contains('selectParkBtn')){
+        const parkCode = event.target.id
+        debugger;
+        getPark(parkCode)
+        .then(_ => {
+            const park = usePark()
+            debugger;
+            dispatchParkEvent(park)
+        })
     }
 })
